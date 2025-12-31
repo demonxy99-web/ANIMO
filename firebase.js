1a1f803b7e06cb5acf9726c854f0500d35049a8e
demonxy99-web <demonxy99@gmail.com>
2025-12-31 23:58:10 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,16 @@
-<script type="module">
-  // Firebase SDKs
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-  import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
-  // Your Firebase config
-  const firebaseConfig = {
-    apiKey: "AIzaSyDRDsqBFjIXp6mh8LO431Usd-Rmpw5pTDM",
-    authDomain: "animo-ab129.firebaseapp.com",
-    projectId: "animo-ab129",
-    storageBucket: "animo-ab129.appspot.com",
-    messagingSenderId: "806187073262",
-    appId: "1:806187073262:web:28fb98cbf9a25a214ffdff"
-  };
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "animo-ab129.firebaseapp.com",
+  projectId: "animo-ab129",
+  storageBucket: "animo-ab129.appspot.com",
+  messagingSenderId: "806187073262",
+  appId: "1:806187073262:web:28fb98cbf9a25a214ffdff"
+};
 
-  // Init Firebase
-  export const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app);
-  export const provider = new GoogleAuthProvider();
-  export const db = getFirestore(app);
-</script>
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
