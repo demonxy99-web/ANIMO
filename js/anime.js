589b4867e52131cba0a3d009b108e714392361c8
demonxy99-web <demonxy99@gmail.com>
2026-01-01 00:35:27 +0530

    Update anime.js

--- js/anime.js
+++ js/anime.js
@@ -15,3 +15,25 @@ snap.forEach(doc => {
   };
   seasonList.appendChild(div);
 });
+import { auth, db } from "../firebase.js";
+import { addDoc, collection } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+
+const favBtn = document.getElementById("favBtn");
+
+if (favBtn) {
+  favBtn.onclick = async () => {
+    const user = auth.currentUser;
+
+    if (!user) {
+      alert("Please login first");
+      return;
+    }
+
+    await addDoc(collection(db, "users", user.uid, "myList"), {
+      animeId: localStorage.getItem("animeId"),
+      addedAt: Date.now()
+    });
+
+    alert("❤️ Added to My List");
+  };
+}
