0006261d84940232c013c15bc5bcda21173399c3
demonxy99-web <demonxy99@gmail.com>
2026-01-01 00:23:30 +0530

    Update player.js

--- js/player.js
+++ js/player.js
@@ -6,3 +6,19 @@ const player = document.getElementById("player");
 if (type === "youtube" || type === "drive") {
   player.src = url;
 }
+import { auth, db } from "../firebase.js";
+import {
+  addDoc, collection, doc, updateDoc, increment
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+
+const user = auth.currentUser;
+if (user) {
+  await addDoc(collection(db, "users", user.uid, "history"), {
+    animeId: localStorage.getItem("animeId"),
+    watchedAt: Date.now()
+  });
+
+  await updateDoc(doc(db, "anime", localStorage.getItem("animeId")), {
+    views: increment(1)
+  });
+}
